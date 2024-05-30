8a18676bf7f894d4b37a3d843c0ff6424fe1ccaa
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-05-30 19:42:42 +0600

    condition add

--- app.js
+++ app.js
@@ -25,3 +25,29 @@ let anotherData;
 
 // cheack data types
 console.log(typeof isTrue);
+
+// Operator start
+let x = 10;
+let y = 20;
+let add = x + y;
+let sub = x - y;
+let mul = x * y;
+let borgo = x ** y;
+let div = x / y;
+let mod = x % 2;
+let inc = x++;
+let dic = y--;
+x += 5;
+x -= 5;
+
+// equal =
+// equal to ==
+// equal value and type ===
+// not equal to !=
+// not equal value or not equal type
+// > greater than
+// < less than
+// >= grater than and equal 
+// <= less than and equal
+// && both should be same
+// || anyone will be same
