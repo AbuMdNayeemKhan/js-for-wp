b32e93b68bc38156f7df3b6aa8a557513e7b01c2
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-05-30 17:03:08 +0600

    javascript

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+// way to print data in console
 console.log('Hello World');
+
+// var
+var fname = "Abu Md.";
+
+// let
+let lname = "Nayeem Khan";
+
+// const
+const fullName = "Abu Md Nayeem Khan";
+
+// string types data
+let stringdata = "Abu Md Nayeem Khan";
+
+// Number types data
+let numberdata = '01619981998';
+
+// boolean
+let isTrue = true;
+let isNotTrue = false;
+
+// Undefined
+let anotherData;
+
+// cheack data types
+console.log(typeof isTrue);
